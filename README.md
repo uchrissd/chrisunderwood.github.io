3aa7e677596e927654c90c4fdc6488aa59c09f18
uchrissd <uchrissd@protonmail.com>
2019-10-05 18:15:13 -0700

    swapped projects in this repo

--- README.md
+++ README.md
@@ -1,48 +1,45 @@
-# Chris Underwood Web Development Portfolio
+# Password Generator
 
 **Motivation**
 
-This is a responsive website built to host my web development portfolio. This is the second portfolio site that I have made. The first version was not responsive. I made this portfolio site as part of an ongoing project with the UCSD web development bootcamp program. This was also an attempt to use Twitter Bootstrap's grid system and responsive classes to reduce the use of custom CSS.
+This is a password generator that I built for a project in the UCSD web development bootcamp program. The goal of the password generator is to give a user the option to choose which characters they would like to include in their password, such as numbers, uppercase letters or special characters, as well as the length of the password. The user can then copy the newly generated password to their clipboard. The application runs on JavaScript and uses Bootstrap to provide styling and responsiveness.
 
 **Build status**
 
-Build status is complete, however there are some things that need to be improved upon.
+The build status is complete.
 
 **Code style**
 
-Standard HTML/CSS and Bootstrap.
+The application uses standard HTML markup style and Bootstrap CSS for styling and responsiveness. The program is written in JavaScript. Comments and descriptions have been added to the code to make it clear for other developers the purpose of each function. I tried to reduce code complexity by breaking down processes into multiple, short and simple functions.
 
 **Screenshots**
 
-![About page screenshot](assets/images/web-dev-about.jpg)
-![Portfolio page screenshot](assets/images/web-dev-portfolio.jpg)
-![Contact page screenshot](assets/images/web-dev-contact.jpg)
+Here are two screenshots of the application.
+
+_Web version_
+
+![Web application screenshot](assets/images/password-generator-full-screenshot.jpg)
+
+_Mobile version_
+
+![Mobile application screenshot](assets/images/password-generator-mobile-screenshot.jpg)
 
 **Code Example**
 
-Here is an example of how I used a mixture of Bootstrap and my own CSS style for this website:
-
-   <!-- <header>
-      <!--This navbar uses a combination of bootstrap and custom style to respon to smaller screens.-->
-
-      <nav class="navbar menu-bar">
-        <div class="name">
-          <h1>Christopher Underwood</h1>
-        </div>
-        <ul class="d-flex portfolio-list">
-          <li style="padding-left: 10px;">
-            <a href="index.html" class="text-secondary">About |</a>
-          </li>
-          <li style="padding-left: 10px;">
-            <a href="portfolio.html" class="text-secondary">Portfolio |</a>
-          </li>
-          <li style="padding-left: 10px;">
-            <a href="contact.html" class="text-secondary"> Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header> -->
+```javascript
+//This function generates the password once the user criteria has been met.
+function generatePassword(validPasswordCharacters, passwordLength) {
+  let password = "";
+  for (let i = 0; i < passwordLength; ++i) {
+    password +=
+      validPasswordCharacters[
+        Math.floor(Math.random() * validPasswordCharacters.length)
+      ];
+  }
+  return password;
+}
+```
 
 **Installation**
 
-No installation necessary. Project is hosted here: https://uchrissd.github.io/chrisunderwood.github.io/
+No installation necessary. Project is hosted here:
---
+++ scripts/script.js
@@ -0,0 +1,96 @@
+//This program is a password generator. User confirms which criteria they want to include in the password though a series of prompts.
+//Program sets lowercase letters as the default in the event a user declines all of the criteria confirmation prompts.
+//The user then chooses a number between 8 and 128 for the length of the password.
+//After they hit the "generate" button, the password appears in the text-area.
+//The "copy to clipboard" button copies the generated password to the clipboard.
+
+//User hits the "generate" button and initiates the program.
+var generateEl = document.getElementById("generate");
+
+generateEl.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  //Constan variables for containing necessary password information for later password generation.
+
+  const validPasswordCharacters = setUpPassWordCharacters();
+  const passwordLength = getPasswordLength();
+  const password = generatePassword(validPasswordCharacters, passwordLength);
+  const passwordEl = document.getElementById("password");
+
+  passwordEl.value = password;
+  //Console logs for debugging the variable values throughout the development process.
+  console.log(validPasswordCharacters);
+  console.log(passwordLength);
+  console.log(password);
+});
+
+//The keys in this object contain the possible characters that can be used in the password.
+let passwordCharacters = [
+  {
+    description: "Do you want uppder case letters in your password?",
+    chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  },
+  {
+    description: "Do you want numbers in your password?",
+    chars: "0123456789"
+  },
+  {
+    description: "Do you want special characters in your password?",
+    chars: ".!@#$%^&*()_+-="
+  }
+];
+
+//This function sets lower case letters as the default for the passowrd and then adds any additional characters based on the user choices.
+function setUpPassWordCharacters() {
+  let validPasswordCharacters = "abcdefghijklmonpqrstuvwxyz";
+  for (let i = 0; i < passwordCharacters.length; ++i) {
+    const passSetting = passwordCharacters[i];
+    const answer = confirm(passSetting.description);
+    if (answer) {
+      validPasswordCharacters += passSetting.chars;
+    }
+  }
+  return validPasswordCharacters;
+}
+
+//This function confirms that the length of the password is between the required 8 and 128 characters.
+function isValidPasswordLength(passwordLength) {
+  return (
+    isNaN(passwordLength) === false &&
+    passwordLength >= 8 &&
+    passwordLength <= 128
+  );
+}
+
+//This function prompts the user to choose how long they want their password to be.
+function getPasswordLength() {
+  let passwordLength = 0;
+  let question =
+    "How long do you want your password to be? Choose a number between 8 and 128.";
+  while (isValidPasswordLength(passwordLength) === false) {
+    passwordLength = parseInt(prompt(question));
+    question = "Uh oh! Choose a number between 8 and 128.";
+  }
+  return passwordLength;
+}
+
+//This function generates the password once the user criteria has been met.
+function generatePassword(validPasswordCharacters, passwordLength) {
+  let password = "";
+  for (let i = 0; i < passwordLength; ++i) {
+    password +=
+      validPasswordCharacters[
+        Math.floor(Math.random() * validPasswordCharacters.length)
+      ];
+  }
+  return password;
+}
+
+//This function allows the user to copy their generated password to the clipboard.
+function copy() {
+  var copyText = document.querySelector("#password");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.querySelector("#copy").addEventListener("click", copy);
